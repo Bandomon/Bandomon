aa417602bea21468bc24de23d17f9466f5fcb589
PedroIFC <ps404412@gmail.com>
2025-04-29 15:52:12 -0300

    Router Ajuste

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/views/Home.vue'
+import Home from '@/views/HomeView.vue'
 import ProductDetail from '@/components/ProductDetail.vue'
 
 const routes = [
